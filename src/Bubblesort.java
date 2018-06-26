b68b3bb1a8b0d4a894b508d19e218ffd3b7a7fc3
tmknok <tiinan.m@gmail.com>
2018-06-26 18:02:27 +0300

    Bubblesort

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -10,28 +10,25 @@ public class Bubblesort {
 	 * @param inputArr Array of int elements which will be sorted.
 	 * @return Array of int elements that has the original array's elements in an increasing order.
 	 */
-	public static int[] bubbleSort(int[] inputArr) {
+	public static void bubbleSort(int[] inputArr) {
 		
 		int length = inputArr.length;
-		
-		for (int pass = 1; pass < length; pass++) {
-			for (int i = 0; i < length-pass; i++) {
-				if (inputArr[i] > inputArr[i + 1]) {
-					int tmp = inputArr[i];
-					inputArr[i] = inputArr[i + 1];						
-					inputArr[i+1] = tmp;
+		boolean swapped;
+		// kay taulukon lapi niin kauan kunnes ei enaa vaihdeta yhtatakaan perakkaista alkiota paittain
+		do {
+			swapped = false;
+			for(int i=1; i < length; i++) {
+				// vertaa kahta perakkaista alkiota toisiinsa, jos epajarjestyksessa -> vaihda paittan
+				if(inputArr[i-1] > inputArr[i]) {
+					int tmp = inputArr[i-1];
+					inputArr[i-1] = inputArr[i];
+					inputArr[i] = tmp;
+					// tallennetaan tieto, etta on tapahtunut vaihdos
+					swapped = true;
 				}
 			}
+			length -= 1;
 			
-		}
-		
-		//Save the sorted array to a new array and return it.
-		int[] result = new int[inputArr.length];
-		int i = 0;
-		for (int element: inputArr) {
-			result[i] = element;
-			i++;
-		}
-		return result;
+		} while (swapped);
 	}
 }
